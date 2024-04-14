05377f83018ceca9e1e674fc4e9d8a6c95e90e67
Dean <dndwiggins@gmail.com>
2024-04-14 14:14:22 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+alert("main.js");
+
+
+//do not have to declare variable types
+
+
+//either use no semi colons or always do
+
+
+let bool = true;
+
+
+let test = 100;
+
+
+if(test > 120){
+
+
+   alert("yo mama");
+}
+
+
+//arrays, like an arraylist, akin to a stack
+//can add multiple types, but generally not preferred
+
+
+
+
+let todos = [];
+
+
+todos.push("do your work ");
+todos.push("wash dishes");
+
+
+//you can use JS to access variables and change their contents
